07ffa3ebf8f2e6ce25cd0bb8d4122c3178d73944
sumnii <sumsong@student.42seoul.kr>
2023-03-05 23:06:30 +0900

    style 수정 적용, 리팩토링

--- js/background.js
+++ js/background.js
@@ -2,13 +2,7 @@ const images = ["0.jpg", "1.jpg", "2.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-// const bgImage = document.createElement("img");
 const body = document.querySelector("body");
 
 body.style.backgroundImage = `url('img/${chosenImage}')`;
 body.style.backgroundSize = "cover";
-
-// bgImage.src = `img/${chosenImage}`;
-// bgImage.className = "background";
-
-// document.body.appendChild(bgImage);
