c379c0358e6bfc266d153fb3d54532d129ba4e18
sumnii <sumsong@student.42seoul.kr>
2023-03-05 23:40:57 +0900

    api key in config.js

--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,11 @@
-const API_KEY = process.env.API_KEY;
+const WEATHER_API = API_KEY;
 const weather = document.querySelector("#weather span:first-child");
 const city = document.querySelector("#weather span:last-child");
 
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API}&units=metric`;
 
   fetch(url)
     .then((response) => response.json())
